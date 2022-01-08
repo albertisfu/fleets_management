7fed2658d6fb986ce4159e2782c6cdecdf2a40e7
Alberto <albertisfu@gmail.com>
2022-01-08 00:08:22 -0600

    fix: change main app name to management

--- management/apps.py
+++ management/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FleetConfig(AppConfig):
+class ManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fleet'
+    name = 'management'
