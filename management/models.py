e91071a82bb5a7516f01a5c48550f4953c03ab51
Alberto <albertisfu@gmail.com>
2022-01-08 22:04:45 -0600

    feat: Vehicle get_travel_history and get_current_location implementation

--- management/models.py
+++ management/models.py
@@ -7,6 +7,27 @@ class Vehicle(models.Model):
     A model to hold vehicle information.
     """
     vehicle_id = models.IntegerField(verbose_name='Vehicle ID',)
+    fuel_level = models.DecimalField(verbose_name='Fuel Level', max_digits=6,
+                                     decimal_places=2, default=0)
+    add_date = models.DateTimeField(verbose_name='Creation Date',
+                                    auto_now_add=True)
+
+    def get_vehicle_travel_history(self):
+        """
+        Get Vechicle Travel History
+        :return: <QuerySet [<VehicleHistory: 1>, <VehicleHistory: 2>]>
+        """
+        history_travel = self.vehiclehistory_set.all()
+        return history_travel
+
+    def get_current_location(self):
+        """
+        Get current vehicle location,
+        latest vehicle history instance register for vehicle
+        :return: city_x
+        """
+        history_travel = self.vehiclehistory_set.all().order_by('-add_date')[0]
+        return history_travel.get_current_location_display()
 
     def __str__(self):
         return str(self.vehicle_id)
@@ -14,7 +35,7 @@ class Vehicle(models.Model):
 
 class VehicleHistory(models.Model):
     """
-    A model to hold vehicle history data, we are going to use 
+    A model to hold vehicle history data, we are going to use
     location, distance and fuel consumed to obtain fuel efficiency (km/lt)
     """
     vehicle = models.ForeignKey(Vehicle,
@@ -23,14 +44,18 @@ class VehicleHistory(models.Model):
     city_a = 0
     city_b = 1
     city_c = 2
-    location_options = ((city_a, 'Desconocido'),
-                        (city_b, 'Bajo'),
-                        (city_c, 'Regular'),)
+    location_options = ((city_a, 'city_a'),
+                        (city_b, 'city_b'),
+                        (city_c, 'city_c'),)
     current_location = models.IntegerField(choices=location_options,
                                            default=city_a,
                                            verbose_name='Current location')
-    distance_traveled = models.IntegerField(verbose_name='Distance Traveled',)
-    fuel_consumed = models.IntegerField(verbose_name='Fuel Consumed',)
+    distance_traveled = models.DecimalField(verbose_name='Travel Distance KM',
+                                            decimal_places=2, max_digits=6,  default=0)
+    fuel_consumed = models.DecimalField(verbose_name='Fuel Consumed LT',
+                                        decimal_places=2, max_digits=6, default=0)
+    add_date = models.DateTimeField(verbose_name='Creation Date',
+                                    auto_now_add=True)
 
     def __str__(self):
         return str(self.pk)
--- management/tests/models_test.py
+++ management/tests/models_test.py
@@ -5,6 +5,24 @@ from management.models import Vehicle, VehicleHistory
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture
+def instance_vehicle(db) -> Vehicle:
+    city_a = 0
+    city_b = 1
+    city_c = 2
+    vehicle = mixer.blend(Vehicle)
+    mixer.blend(VehicleHistory, vehicle=vehicle,
+                current_location=city_a,
+                distance_traveled=0, fuel_consumed=0)
+    mixer.blend(VehicleHistory, vehicle=vehicle,
+                current_location=city_b,
+                distance_traveled=20, fuel_consumed=10)
+    mixer.blend(VehicleHistory, vehicle=vehicle,
+                current_location=city_c,
+                distance_traveled=30, fuel_consumed=25)
+    return vehicle
+
+
 class TestVehicle:
     def test_model(self):
         """
@@ -12,6 +30,21 @@ class TestVehicle:
         """
         vehicle = mixer.blend(Vehicle)
         assert vehicle.pk == 1, 'Create a Vehicle instance'
+
+    def test_get_vehicle_travel_history(self, instance_vehicle: Vehicle):
+        """
+        Test get_vehicle_travel_history function,
+        it has to match with 3 instances created in fixtures
+        """
+        history = instance_vehicle.get_vehicle_travel_history()
+        # print('history cities', history)
+        # print('history cities', [x.current_location for x in history])
+        assert history.count() == 3, 'Check if it return 3 instances created'
+
+    def test_get_current_location(self, instance_vehicle: Vehicle):
+        current_location = instance_vehicle.get_current_location()
+        assert current_location == 'city_c', \
+            'Check if current location match with latest history created'
 
 
 class TestVehicleHistory:
