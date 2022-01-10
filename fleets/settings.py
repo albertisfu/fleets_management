b1c824a9da383c92eb3990039032e28371081ef8
Alberto <albertisfu@gmail.com>
2022-01-10 02:15:22 -0600

    fix: docker database connection

--- fleets/settings.py
+++ fleets/settings.py
@@ -88,10 +88,22 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'ndd',
+        'USER': 'root',
+        'PASSWORD': 'ndd',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
